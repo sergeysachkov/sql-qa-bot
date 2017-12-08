19faf5a8673a5303059d427826037d7f8adefa30
esersac <sergey.sachkov@ericsson.com>
2017-12-08 14:36:51 +0000

    updating docker file to be able to start test worker

--- kaldi-docker/src/main/docker/start_kaldi.sh
+++ kaldi-docker/src/main/docker/start_kaldi.sh
@@ -2,5 +2,5 @@
 
 python /opt/kaldi-gstreamer-server/kaldigstserver/master_server.py --port=8888 2>> /opt/master.log &
 
-python /opt/kaldi-gstreamer-server/kaldigstserver/worker.py -u ws://localhost:8888/worker/ws/speech -c sample_worker.yaml 2>> /opt/worker.log &
+python /opt/kaldi-gstreamer-server/kaldigstserver/worker.py -u ws://localhost:8888/worker/ws/speech -c /opt/kaldi-gstreamer-server/sample_worker.yaml 2>> /opt/worker.log &
 
